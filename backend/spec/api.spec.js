547d6496fadeef22f3ca9271dd12eda15bbf2abc
Mihaita Ivli <mihaitaivli@gmail.com>
2018-04-16 14:04:10 +0100

    Test the app mounts the general handlers

--- backend/spec/api.spec.js
+++ backend/spec/api.spec.js
@@ -5,7 +5,8 @@ const request = require('supertest');
 const expect = require('chai').expect;
 
 describe('-----API endpoints-----', () => {
-  describe('/ - GET', () => {
+  
+  describe('/', () => {
     it('responds with a 200 status', (done) => {
       request(app)
         .get('/')
@@ -15,4 +16,47 @@ describe('-----API endpoints-----', () => {
         });
     });
   });
+
+  describe('/api - endpoint', () => {
+    it('responds with 200 on /apply ', (done) => {
+      request(app)
+        .get('/api/apply')
+        .end((req, res) => {
+          expect(res.status).to.equal(200);
+          done();
+        })
+    })
+    it('responds with 200 on /signup ', (done) => {
+      request(app)
+        .get('/api/signup')
+        .end((req, res) => {
+          expect(res.status).to.equal(200);
+          done();
+        })
+    })
+    it('responds with 200 on /signin ', (done) => {
+      request(app)
+        .get('/api/signin')
+        .end((req, res) => {
+          expect(res.status).to.equal(200);
+          done();
+        })
+    })
+    it('responds with 200 on /reject ', (done) => {
+      request(app)
+        .get('/api/reject')
+        .end((req, res) => {
+          expect(res.status).to.equal(200);
+          done();
+        })
+    })
+    it('responds with 200 on /contact ', (done) => {
+      request(app)
+        .get('/api/contact')
+        .end((req, res) => {
+          expect(res.status).to.equal(200);
+          done();
+        })
+    })
+  })
 });
