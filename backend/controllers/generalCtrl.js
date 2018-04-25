96a24ac4f772719a53dcb8330fa3e195eb0eb5e9
Mihaita Ivli <mihaitaivli@gmail.com>
2018-04-25 15:48:08 +0100

    WIP implement signing in functionality

--- backend/controllers/generalCtrl.js
+++ backend/controllers/generalCtrl.js
@@ -1,7 +1,11 @@
+const Customers = require('../models/customers');
 const checkEligibility = require('../lib/checkEligibility');
 
 module.exports = {
   apply(data, cb){
     return cb(null, checkEligibility(data.amount, data.duration));
+  },
+  signin(data, cb){
+    // TODO
   }
 }
--- backend/routers/general/signinRouter.js
+++ backend/routers/general/signinRouter.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const signinRouter = express.Router();
+const generalCtrl = require('../../controllers/generalCtrl');
 
-signinRouter.get('/', (req, res) => {
-  res.send('On the /signin endpoint');
+signinRouter.post('/', (req, res, next) => {
+  generalCtrl.signin(req.body, (err, status) => {
+    if (err) {
+      err.status = 500;
+      next(err);
+    } else res.send(status);
+  });
 });
 
 module.exports = signinRouter;
