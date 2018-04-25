9b299d5cb93c874560ecc848050835052c0c6ecf
Mihaita Ivli <mihaitaivli@gmail.com>
2018-04-25 20:50:47 +0100

    Remove the signin route

--- backend/routers/generalRouter.js
+++ backend/routers/generalRouter.js
@@ -3,12 +3,10 @@ const generalRouter = express.Router();
 
 const applyRouter = require('./general/applyRouter');
 const signupRouter = require('./general/signupRouter');
-const signinRouter = require('./general/signinRouter');
 const variablesRouter = require('./general/variablesRouter');
 
 generalRouter.use('/apply', applyRouter);
 generalRouter.use('/signup', signupRouter);
-generalRouter.use('/signin', signinRouter);
 generalRouter.use('/variables', variablesRouter);
 
 module.exports = generalRouter;
