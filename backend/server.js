3fe9ead2884420a94a513fb8127d3171320a7c37
Mihaita Ivli <mihaitaivli@gmail.com>
2018-06-04 21:53:07 +0100

    Use the cors module in the server

--- backend/server.js
+++ backend/server.js
@@ -3,21 +3,15 @@ if (!process.env.NODE_ENV) process.env.NODE_ENV = 'dev';
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const generalRouter = require('./routers/generalRouter');
 const adminRouter = require('./routers/adminRouter');
 const errorRouter = require('./routers/errorRouter');
 const errorHandler = require('./lib/errorHandler');
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
-
 app.use(bodyParser.json());
-
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('api working...');
