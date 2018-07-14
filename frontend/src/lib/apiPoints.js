2103c98d22a7d52b3ed6ac94e027038441a9bd8b
mihaitaivli <mihaitaivli@gmail.com>
2018-07-14 17:41:25 +0100

    Update the mail api endpoint to a generic one

--- frontend/src/lib/apiPoints.js
+++ frontend/src/lib/apiPoints.js
@@ -3,5 +3,5 @@ module.exports = {
   loans: 'http://localhost:3001/api/admin/loans/',
   login: 'http://localhost:3001/api/login',
   creditScore: 'https://hpri26omx7.execute-api.eu-west-2.amazonaws.com/stage2/returnCreditScore',
-  mailAPI: 'https://mail.zoho.eu/api/accounts/2562974000000008001/messages' 
+  mailAPI: 'https://mail.zoho.eu/api/YOUR-MAIL-API-ENDPOINT' 
 }
